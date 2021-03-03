89611bc173311b0741dde5d7fa76f8ebd72b91a7
Thiago Rossato <thiago.rossato.tr@gmail.com>
2021-03-03 18:13:08 -0300

    feat: Estudo SOLID - Cadastro e Email de boas vindas

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # principios-solid
+
+Estudo sobre SOLID.
+
+
+S - Uma classe deve ter um, e somente um, motivo para mudar.
+O - Você deve ser capaz de estender um comportamento de uma classe, sem modificá-lo.
+L - As classes base devem ser substituíveis por suas classes derivadas.
+I - Muitas interfaces específicas são melhores do que uma interface única.
+D - Dependa de uma abstração e não de uma implementação.
---
+++ src/useCases/CreateUser/index.ts
@@ -0,0 +1,18 @@
+import { MailTrapMailProvider } from "../../providers/implementations/MailtrapMailProvider";
+import { PostgresUsersRepository } from "../../repositories/implementations/PostgresUsersRepository";
+import { CreateUserController } from "./CreateUserController";
+import { CreateUserUseCase } from "./CreateUserUseCase";
+
+const mailtrapMailProvider = new  MailTrapMailProvider();
+const postgresUsersRepository = new PostgresUsersRepository();
+
+const createUserUseCase = new CreateUserUseCase(
+    postgresUsersRepository,
+    mailtrapMailProvider
+);
+
+const createUserController = new CreateUserController(
+    createUserUseCase
+);
+
+export { createUserUseCase, createUserController};
